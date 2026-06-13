8e2131b6b70e6a83c05b293cbde2cdbc7420e464
colinalt123123 <colinalt123123@gmail.com>
2026-06-13 22:37:11 +0200

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#B30000", 
+      secondary: "#4f1b9d", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
